2db1ff9e345421957da5fb34cdb33032eaa77806
Steven Zgaljic <szgaljic@jahnelgroup.com>
2018-02-12 22:28:35 -0600

    Added libs for QueryDSL although it's not supported yet

--- jgBay/search/src/main/java/com/jahnelgroup/jgbay/search/data/auction/AuctionRepo.java
+++ jgBay/search/src/main/java/com/jahnelgroup/jgbay/search/data/auction/AuctionRepo.java
@@ -1,7 +1,20 @@
 package com.jahnelgroup.jgbay.search.data.auction;
 
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface AuctionRepo extends ElasticsearchRepository<Auction, Long> {
+/**
+ * As of 2/12/18 QueryDSL is not supported with Spring Data ES yet.
+ *
+ * https://jira.spring.io/browse/DATAES-219
+ *
+ * https://github.com/spring-projects/spring-data-elasticsearch/blob/d30b41cc5977118a949dbf887c66d3fa87e77fb2/src/main/java/org/springframework/data/elasticsearch/repository/support/ElasticsearchRepositoryFactory.java#L79-L81
+ *
+ */
+public interface AuctionRepo extends ElasticsearchRepository<Auction, Long>
+
+        /*, QuerydslPredicateExecutor<Auction>*/
+
+        {
 
 }
--- jgBay/search/src/main/java/com/jahnelgroup/jgbay/search/rest/RestConfig.java
+++ jgBay/search/src/main/java/com/jahnelgroup/jgbay/search/rest/RestConfig.java
@@ -1,4 +1,4 @@
-package com.jahnelgroup.jgbay.search.data.rest;
+package com.jahnelgroup.jgbay.search.rest;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
