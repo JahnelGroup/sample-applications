50400c0406a9069d3fdcf2993d761821200fd9ac
Joe Chiavaroli <jchiavaroli@jahnelgroup.com>
2018-02-16 10:46:06 -0500

    Fixed .gitignore and missin gifles

---
+++ jgBay/search/src/main/java/com/jahnelgroup/jgbay/search/data/index/JsonNodeResourceAssembler.java
@@ -0,0 +1,17 @@
+package com.jahnelgroup.jgbay.search.data.index;
+
+import org.springframework.hateoas.ResourceSupport;
+import org.springframework.hateoas.mvc.IdentifiableResourceAssemblerSupport;
+
+public class JsonNodeResourceAssembler extends IdentifiableResourceAssemblerSupport<JsonNodeDocument, ResourceSupport> {
+
+    public JsonNodeResourceAssembler() {
+        super(IndexController.class, ResourceSupport.class);
+    }
+
+    @Override
+    public ResourceSupport toResource(JsonNodeDocument entity) {
+        return null;
+    }
+
+}
